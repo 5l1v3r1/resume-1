b44e3bacdf11e96c19d8ae98bf1a627d78ec815f
tgrrr <phil@tgrrr.com>
2018-01-25 22:15:15 +1100

    alignment

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -28,33 +28,31 @@ const Education = ({jobs}) => (
         <h1>Education</h1>
       </Col>
     </Row>
-    {
-      jobs.map((job, i) => (
-        <div key={i}>
-          {job.fields.institution ?
-            <Row style={muiTheme.education}>
-              <Col xs={12} sm={8}>
-                <strong>{job.fields.institution['en-US']}</strong>
-              </Col>
-              <Col xs={12} sm={4}>
-                <Row end="sm">
-                  <Col xs>
-                    {job.fields.yearRange['en-US']}
-                  </Col>
-                </Row>
-              </Col>
-              <Col xs={12}>
-                {job.fields.qualification['en-US']}
-              </Col>
-              <Col xs={12}>
-                <i>{job.fields.focus ? job.fields.focus['en-US'] : null }</i>
-              </Col>
-            </Row>
-            : null
-          }
-        </div>
-      ))
-    }
+    {jobs.map((job, i) => (
+      <div key={i}>
+        {job.fields.institution ?
+          <Row style={muiTheme.education}>
+            <Col xs={12} sm={8}>
+              <strong>{job.fields.institution['en-US']}</strong>
+            </Col>
+            <Col xs={12} sm={4}>
+              <Row end="sm">
+                <Col xs>
+                  {job.fields.yearRange['en-US']}
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={12}>
+              {job.fields.qualification['en-US']}
+            </Col>
+            <Col xs={12}>
+              <i>{job.fields.focus ? job.fields.focus['en-US'] : null }</i>
+            </Col>
+          </Row>
+          : null
+        }
+      </div>
+    ))}
   </div>
 );
 
