20c99f2a018119772871a4e5a6328564fc9eb7cf
tgrrr <phil@tgrrr.com>
2018-01-31 15:51:59 +1100

    Removed profilePhoto require

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-flexbox-grid';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import RaisedButton from 'material-ui/RaisedButton';
+import profilePhoto from '../../images/philsteinke mugshot bw.png';
 
 const muiTheme = getMuiTheme({
-  mugshot: {
+  profilePhoto: {
     width: 150,
     height: 150,
     borderRadius: 150,
@@ -30,9 +31,9 @@ const Header = () => (
     <Col xs={12} sm={4} md={4} lg={4}>
       <Row center="xs">
         <img
-          src={require(`../../images/philsteinke mugshot bw.png`)}
+          src={profilePhoto}
           alt="Phil Steinke"
-          style={muiTheme.mugshot}
+          style={muiTheme.profilePhoto}
         />
       </Row>
     </Col>
