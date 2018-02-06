9af91f98c049eab92e31e906447aa2c93793b845
tgrrr <phil@tgrrr.com>
2018-02-06 13:24:28 +1100

    refactored contact details to cms

--- src/components/Header/ContactModal.jsx
+++ src/components/Header/ContactModal.jsx
@@ -5,6 +5,7 @@ import Phone from 'material-ui/svg-icons/communication/phone';
 import MapPointer from 'material-ui/svg-icons/maps/place';
 import Email from 'material-ui/svg-icons/communication/email';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import ResumeBaseTheme from '../../theme/resumeBaseTheme';
 
 const muiTheme = getMuiTheme({
   raisedButton: {
@@ -12,35 +13,20 @@ const muiTheme = getMuiTheme({
   }
 });
 
-const ContactModal = () => (
-  <div>
-    <h3> Get in Touch </h3>
-    <Row center="xs">
+const ContactModal = props => (
+  props.entries
+    .filter(entry => entry.sys.contentType.sys.id === 'contact')
+    .map(entry => (
       <RaisedButton
-        label="0403989033"
+        key={entry.sys.id}
+        label={entry.fields.name && entry.fields.name['en-US']}
         labelPosition="after"
-        icon={<Phone />}
+        // icon={entry.fields.icon && entry.fields.icon['en-US']}
         secondary
+        color="white"
         style={muiTheme.raisedButton}
-        href="tel:+61403989033"
+        href={entry.fields.link && entry.fields.link['en-US']}
       />
-      <RaisedButton
-        label="phil@tgrrr.com"
-        labelPosition="after"
-        icon={<Email />}
-        secondary
-        style={muiTheme.raisedButton}
-        href="mailto:phil@tgrrr.com"
-      />
-      <RaisedButton
-        label="Melbourne, Australia"
-        labelPosition="after"
-        icon={<MapPointer />}
-        style={muiTheme.raisedButton}
-        href="https://www.google.com.au/maps/place/1%2F377+Sydney+Rd,+Coburg+VIC+3058/@-37.7446427,144.9636139,17z/data=!3m1!4b1!4m5!3m4!1s0x6ad644b3c1b5fdeb:0x5307945657c3381b!8m2!3d-37.744647!4d144.9658026"
-      />
-    </Row>
-  </div>
-);
+    )));
 
 export default ContactModal;
