12d8edeff2c213e2653ce7e9c76df159a0ae5cc6
tgrrr <phil@tgrrr.com>
2018-02-06 13:27:40 +1100

    Added frontend dev to title

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-flexbox-grid';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import RaisedButton from 'material-ui/RaisedButton';
+import ResumeBaseTheme from '../../theme/resumeBaseTheme';
 
 const muiTheme = getMuiTheme({
   profilePhoto: {
@@ -22,12 +23,14 @@ const muiTheme = getMuiTheme({
 });
 
 const Header = () => (
-  // <Row center="xs">
   <Row>
-    <Col xs={12} sm={6} md={6} lg={6}>
-      <h1> Phil Steinke </h1>
+    <Col xs={12} sm={6} md={2} lg={2}>
+      <h1>Phil Steinke</h1>
     </Col>
     <Col xs={12} sm={6} md={6} lg={6}>
+      <h1 style={{ color: ResumeBaseTheme.palette.primary1Color }}>Frontend Developer</h1>
+    </Col>
+    <Col xs={12} sm={6} md={4} lg={4}>
       <RaisedButton
         containerElement={<Link to="/contact" />}
         label="Get in touch"
