ed9538bdd987e677447620c563ab9945dafb898d
tgrrr <phil@tgrrr.com>
2018-02-09 01:21:13 +1100

    achievement props

--- src/components/Jobs/Achievements.jsx
+++ src/components/Jobs/Achievements.jsx
@@ -5,9 +5,9 @@ const Achievements = ({ achievement1, achievement2, achievement3 }) => (
   <div>
     <h4>Achievements</h4>
     <ul>
-      { achievement1 && <li> {achievement1['en-US']}</li>}<p />
-      { achievement2 && <li> {achievement2['en-US']}</li>}<p />
-      { achievement3 && <li> {achievement3['en-US']}</li>}<p />
+      { achievement1 && <li> {achievement1}</li>}<p />
+      { achievement2 && <li> {achievement2}</li>}<p />
+      { achievement3 && <li> {achievement3}</li>}<p />
     </ul>
   </div>
 );
