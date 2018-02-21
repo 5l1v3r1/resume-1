262585b4014e220f36ac62db72b7a6150f28f67e
tgrrr <phil@tgrrr.com>
2018-02-21 13:35:51 +1100

    Fixed acheivement extra bullet points

--- src/components/Jobs/Achievements.jsx
+++ src/components/Jobs/Achievements.jsx
@@ -5,9 +5,9 @@ const Achievements = ({ achievement1, achievement2, achievement3 }) => (
   <div>
     <h4>Achievements</h4>
     <ul>
-      { achievement1 && <li> {achievement1}</li>}<p />
-      { achievement2 && <li> {achievement2}</li>}<p />
-      { achievement3 && <li> {achievement3}</li>}<p />
+      { achievement1 && <li> {achievement1}</li>}
+      { achievement2 && <li> {achievement2}</li>}
+      { achievement3 && <li> {achievement3}</li>}
     </ul>
   </div>
 );
